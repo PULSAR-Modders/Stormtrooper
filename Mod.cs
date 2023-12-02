e9905279cfcc03aa15a542b901962642a0027599
18107 <18107@users.noreply.github.com>
2023-12-02 11:15:11 +1030

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Stormtrooper
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Significantly reduces the player's accuracy";
+
+        public override string Name => "Stormtrooper";
+
+        public override string ModID => "stormtrooper";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.stormtrooper";
+        }
+    }
+}
---
+++ Pawn.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace Stormtrooper
+{
+    [HarmonyPatch(typeof(PLPawn), "Update")]
+    internal class Pawn
+    {
+        static void Postfix(PLPawn __instance)
+        {
+            if (__instance != PLNetworkManager.Instance.LocalPlayer.GetPawn()) return;
+            __instance.CurrentAccuracyRating = 100000f;
+        }
+    }
+}
